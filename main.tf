c910d57ef576afe795adebf8c5fc4e7d585ecd19
Sergey Lavrov <35638053+amentum93@users.noreply.github.com>
2024-05-29 14:57:03 +0300

    terraform

---
+++ main.tf
@@ -0,0 +1,141 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-b"
+  service_account_key_file = "${file("./service-account-key.json")}"
+  folder_id = "b1grn43t7am6uetv5o7s" 
+}
+
+resource "yandex_compute_disk" "boot-disk-1" {
+  name     = "boot-disk-1"
+  type     = "network-hdd"
+  zone     = "ru-central1-b"
+  size     = "30"
+  image_id = "fd89jk9j9vifp28uprop"
+}
+
+resource "yandex_compute_disk" "boot-disk-2" {
+  name     = "boot-disk-2"
+  type     = "network-hdd"
+  zone     = "ru-central1-b"
+  size     = "30"
+  image_id = "fd89jk9j9vifp28uprop"
+}
+
+resource "yandex_compute_disk" "boot-disk-3" {
+  name     = "boot-disk-3"
+  type     = "network-hdd"
+  zone     = "ru-central1-b"
+  size     = "30"
+  image_id = "fd89jk9j9vifp28uprop"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "srv"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-1.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./config-cloud")}"
+  }
+}
+
+
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "master"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-2.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./config-cloud")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-3" {
+  name = "app"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-3.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./config-cloud")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_3" {
+  value = yandex_compute_instance.vm-3.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_3" {
+  value = yandex_compute_instance.vm-3.network_interface.0.nat_ip_address
+}
